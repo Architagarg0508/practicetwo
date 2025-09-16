0f21ba32657ccd7b554e2e1f6c13f3e89766e5f4
Architagarg0508 <gargarchita@gmail.com>
2025-09-16 16:57:36 +0530

    img change to svg

--- src/app/components/Orange.js
+++ src/app/components/Orange.js
@@ -3,26 +3,25 @@ import Image from "next/image";
 export default function Orange() {
   return (
     <section className={styles.orangeSection}>
-        <picture className={styles.topline}>
-            <Image 
-              src="/Vector48.svg" 
-              alt="curve"
-              height={0}
-              width={0}
-              className={styles.curve1}
-           />
-        </picture>
+      <picture className={styles.topline}>
+        <Image
+          src="/Vector48.svg"
+          alt="curve"
+          height={0}
+          width={0}
+          className={styles.curve1}
+        />
+      </picture>
       {/* <img src="/Vector48.svg" className={styles.curve1} alt="curve" /> */}
 
-      
-        <h1 className={styles.mainTextHeading}>
-          HAPPINESS is <br /> <span>NOT A GOAL.</span>
-        </h1>
-        <p className={styles.mainTextPara}>
-          It is an outcome. <br />
-          An outcome of a life with meaning.
-        </p>
-      
+      <h1 className={styles.mainTextHeading}>
+        HAPPINESS is <br /> <span>NOT A GOAL.</span>
+      </h1>
+      <p className={styles.mainTextPara}>
+        It is an outcome. <br />
+        An outcome of a life with meaning.
+      </p>
+
       <div className={styles.cards}>
         <div className={styles.card}>
           <h2>THE INITIATIVE</h2>
@@ -40,10 +39,34 @@ export default function Orange() {
             <div className={styles.Box}>
               <div className={styles.arrowCircle}>
                 <span className={styles.arrow}>
-                  <img src="/Arrows.svg" alt="arrow" />
+                  <svg
+                    width="19"
+                    height="18"
+                    viewBox="0 0 19 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9 0.515625L17.4853 9.00091L9 17.4862"
+                      stroke="black"
+                    />
+                    <path d="M0.514719 9.00091H17.4853" stroke="black" />
+                  </svg>
                 </span>
                 <span className={styles.arrow2}>
-                  <img src="/Arrows.svg" alt="arrow" />
+                  <svg
+                    width="19"
+                    height="18"
+                    viewBox="0 0 19 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9 0.515625L17.4853 9.00091L9 17.4862"
+                      stroke="black"
+                    />
+                    <path d="M0.514719 9.00091H17.4853" stroke="black" />
+                  </svg>
                 </span>
               </div>
             </div>
@@ -65,27 +88,49 @@ export default function Orange() {
             <div className={styles.Box}>
               <span className={styles.arrowCircle}>
                 <span className={styles.arrow}>
-                  <img src="/Arrows.svg" alt="arrow" />
+                  <svg
+                    width="19"
+                    height="18"
+                    viewBox="0 0 19 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9 0.515625L17.4853 9.00091L9 17.4862"
+                      stroke="black"
+                    />
+                    <path d="M0.514719 9.00091H17.4853" stroke="black" />
+                  </svg>
                 </span>
                 <span className={styles.arrow2}>
-                  <img src="/Arrows.svg" alt="arrow" />
+                  <svg
+                    width="19"
+                    height="18"
+                    viewBox="0 0 19 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9 0.515625L17.4853 9.00091L9 17.4862"
+                      stroke="black"
+                    />
+                    <path d="M0.514719 9.00091H17.4853" stroke="black" />
+                  </svg>
                 </span>
               </span>
             </div>
           </a>
         </div>
-        
       </div>
       <picture className={styles.bottomline}>
-            <Image 
-              src="/Vector47.svg" 
-              alt="curve"
-              height={0}
-              width={0}
-              className={styles.curve2}
-           />
-        </picture>
-     
+        <Image
+          src="/Vector47.svg"
+          alt="curve"
+          height={0}
+          width={0}
+          className={styles.curve2}
+        />
+      </picture>
     </section>
   );
 }
